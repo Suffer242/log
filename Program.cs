66b214df4778b8112cac31c1ca6f3aed2c6de913
Suffer242 <wump@yandex.ru>
2018-10-13 23:13:44 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace log
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var o = System.IO.File.ReadAllLines(@"e:\Games\Eagle Dynamics\Logfile.CSV.txt")
+            .Where(f=>f.Contains("\",\"Reg") && f.ToLower().Contains("write") );
+
+            System.IO.File.WriteAllLines(@"e:\Games\Eagle Dynamics\Logfile.CSV.txt.txt",o);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
